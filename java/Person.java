de391ea2407a3202f2d758c42bea4a7593f3e2f7
23alex39 <126571768+23alex39@users.noreply.github.com>
2023-03-14 08:19:47 +0100

    Java.Person

---
+++ java/Person.java
@@ -0,0 +1,23 @@
+public class Person {
+    public static void main(String[] args) {
+
+
+    int year0fBirth=1973;
+    String a = "Name";
+    String b = "Surname";
+    int year = 49;
+    int size = 48;
+        System.out.println(a);
+
+    }
+
+
+
+
+
+
+
+
+
+    }
+
